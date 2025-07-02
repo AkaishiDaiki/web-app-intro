8bac393f59cf782938c5b5726b6e02ab5c4b2afa
AkaishiDaiki <sdt2495wh0001@edu.sca.ac.jp>
2025-07-02 09:19:15 +0000

    ??

--- final_project/my_project/script.js
+++ final_project/my_project/script.js
@@ -18,6 +18,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 checkbox.type = 'checkbox';
                 checkbox.id = `item-${item.id}`;
                 const listItem = document.createElement('li');
+                listItem.appendChild(checkbox);
                 listItem.textContent = `ID: ${item.id}, 課題内容 ${item.value_1}, 期日 ${item.value_2 || 'N/A'}`;
                 dataList.appendChild(listItem);
             });
