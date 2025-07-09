f072c4405db43447ad6f34fa86cd1ec1091b4ba5
AkaishiDaiki <sdt2495wh0001@edu.sca.ac.jp>
2025-07-09 07:57:26 +0000

    ほんとにお願い

--- final_project/my_project/script.js
+++ final_project/my_project/script.js
@@ -19,7 +19,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 checkbox.id = `item-${item.id}`;
                 const listItem = document.createElement('li');
                 listItem.appendChild(checkbox);
-                listItem.appendChild(document.createTextNode(`課題 ${item.id}, 課題内容 ${item.value_1  || ''}`));
+                listItem.appendChild(document.createTextNode(`課題 ${item.id}, 課題内容 ${item.value_1}, 期日 ${item.value_2  || ''}`));
                 dataList.appendChild(listItem);
             });
         } catch (error) {
